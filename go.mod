f3c697b853f52b2fe7690830b96e00f66bd1c524
xjh <threeax@foxmail.com>
2024-10-18 12:39:52 +0800

    feat: 重构 Core 结构，添加 CoreConf 配置

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/kohmebot/kohme
 go 1.20
 
 require (
-	github.com/kohmebot/plugin v1.1.0
+	github.com/kohmebot/plugin v1.1.2
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/wdvxdr1123/ZeroBot v1.7.5
@@ -18,9 +18,11 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/panjf2000/ants/v2 v2.10.0 // indirect
 	github.com/tidwall/gjson v1.18.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 )
--- internal/app/core.go
+++ internal/app/core.go
@@ -3,42 +3,89 @@ package app
 import (
 	"fmt"
 	"github.com/kohmebot/plugin"
+	"github.com/kohmebot/plugin/pkg/chain"
 	"github.com/kohmebot/plugin/pkg/command"
+	"github.com/kohmebot/plugin/pkg/gopool"
 	"github.com/kohmebot/plugin/pkg/version"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
-	"strings"
 )
 
 var v = version.NewVersion(0, 0, 1)
 
+type CoreConf struct {
+	HelpTop  string `yaml:"help_top" mapstructure:"help_top"`
+	HelpTail string `yaml:"help_tail" mapstructure:"help_tail"`
+}
+
 type Core struct {
-	app *App
+	app  *App
+	conf CoreConf
 }
 
 func newCore(a *App) *Core {
 	return &Core{
-		a,
+		app: a,
 	}
 }
 
 func (c *Core) Init(engine *zero.Engine, env plugin.Env) error {
-	// FIXME 功能待完善。暂时关闭
-	g := env.Groups()
+
+	err := env.GetConf(&c.conf)
+	if err != nil {
+		return err
+	}
+	err = c.onHelp(engine, env)
+	if err != nil {
+		return err
+	}
+	err = c.onManager(engine, env)
+	if err != nil {
+		return err
+	}
 	return nil
-	engine.OnCommandGroup([]string{"help", "?", "？"}, g.Rule(func(ctx *zero.Ctx) bool {
-		var builder strings.Builder
+}
+
+func (c *Core) onHelp(engine *zero.Engine, env plugin.Env) error {
+	g := env.Groups()
+	prefix := c.app.opt.AppConf.Zero.CommandPrefix
+	engine.OnCommandGroup([]string{"help", "?", "？", "帮助"}, g.Rule(func(ctx *zero.Ctx) bool {
+		var msgChain chain.MessageChain
+		msgChain.Split(message.Text(c.conf.HelpTop), message.Text(fmt.Sprintf(`命令前缀 "%s"`, prefix)))
+		msgChain.Line()
 		for _, p := range c.app.pluginMp {
-			if p.Name() == c.Name() {
+			pEnv := c.app.envMp[p.Name()]
+			// 跳过关闭的插件
+			if pEnv.disable.Load() {
 				continue
 			}
-			builder.WriteString(fmt.Sprintf("%s (%s)\n", p.Name(), p.Description()))
-			builder.WriteString(p.Commands().String())
-			builder.WriteByte('\n')
+
+			msgChain.Line(message.Text(fmt.Sprintf("🌟%s (%s)", p.Name(), p.Description())))
+			msgChain.Join(message.Text(p.Commands().String()))
 		}
-		go ctx.Send(message.Text(builder.String()))
+		msgChain.Split(message.Text("-----"), message.Text(c.conf.HelpTail))
+		gopool.Go(func() {
+			ctx.Send(msgChain)
+		})
+
 		return true
 	}))
+	return nil
+}
+
+func (c *Core) onManager(engine *zero.Engine, env plugin.Env) error {
+	supers := c.app.opt.AppConf.Zero.SuperUsers
+	engine.OnCommand("ping", func(ctx *zero.Ctx) bool {
+		for _, super := range supers {
+			if super == ctx.Event.Sender.ID {
+				gopool.Go(func() {
+					ctx.Send(message.Text("pong!我还活着"))
+				})
+				break
+			}
+		}
+		return true
+	})
 	return nil
 }
 
@@ -47,12 +94,12 @@ func (c *Core) Name() string {
 }
 
 func (c *Core) Description() string {
-	return "管理装载的插件"
+	return "核心插件"
 }
 
 func (c *Core) Commands() command.Commands {
 	return command.NewCommands(
-		command.NewCommand("查看帮助", "help", "?", "？"),
+		command.NewCommand("查看帮助", "help", "?", "帮助"),
 	)
 }
 
